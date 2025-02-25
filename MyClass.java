e25c198b3ac7e9570c4eb13e4b2f75e1d295440f
arpitparekh <arpitparekh9@gmail.com>
2025-02-25 19:55:43 +0530

    first commit

---
+++ MyClass.java
@@ -0,0 +1,22 @@
+public class MyClass{
+
+  public static void main(String[] args) {
+
+    // in java everything is a class
+
+    // hello World
+    // println()
+    System.out.println("Hello World"); // auto imported
+
+    // sum
+    int a = 10;
+    int b = 20;
+
+    int c = a + b;
+
+    System.out.println(c);
+
+
+  }
+
+}
